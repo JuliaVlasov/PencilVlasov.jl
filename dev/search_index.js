a153e9800d3c145e83928770e51244f62b122303
Documenter.jl <documenter@juliadocs.github.io>
2022-11-01 14:24:38 +0000

    build based on 6879fdb

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#PencilVlasov.jl","page":"PencilVlasov.jl","title":"PencilVlasov.jl","text":"","category":"section"},{"location":"","page":"PencilVlasov.jl","title":"PencilVlasov.jl","text":"Documentation for PencilVlasov.jl","category":"page"}]
+}
